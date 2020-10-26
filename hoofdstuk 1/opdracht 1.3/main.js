643c4862e1b34307fae6224fe1f7a93cdc61b17d
Kevin <kgyori@roc-dev.com>
2020-10-26 13:43:48 +0100

    init commit

---
+++ hoofdstuk 1/opdracht 1.3/main.js
@@ -0,0 +1,8 @@
+var mijnauto = {
+    merk: "ford",
+    type: "Monedo",
+    aantalWielen: 4,
+    kleur: "blauw"
+}
+
+console.log (mijnauto.kleur);
---
+++ hoofdstuk 2/opdracht2.2/main.js
@@ -0,0 +1,13 @@
+document.getElementById("bl1").style.backgroundColor = "blue";
+document.getElementById("bl2").style.backgroundColor = "green";
+document.getElementById("bl3").style.backgroundColor = "yellow";
+document.getElementById("bl4").style.backgroundColor = "red";
+
+document.getElementById("bl2").style.position = "relative";
+document.getElementById("bl3").style.position = "relative";
+
+
+if (bl2,bl3) {
+    document.getElementById("bl2").style.top = "10px";
+    document.getElementById("bl3").style.top = "30px";
+}
