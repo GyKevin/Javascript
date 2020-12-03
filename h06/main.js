4eac76ef9c1c7d2fabaefd0af18a7829bcbeb915
Kevin <kgyori@roc-dev.com>
2020-12-03 11:39:21 +0100

    memory game afgemaakt

--- h06/main.js
+++ h06/main.js
@@ -1,16 +1,21 @@
+//Flip Cards
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
     this.classList.toggle('flip');
     if (!hasFlippedCard) {
         hasFlippedCard = true;
         firstCard = this;
     } else {
-        hasFlippedCard = false;
         secondCard = this;
+
+        checkForMatch();
     }
 }
 
@@ -25,13 +30,34 @@ function checkForMatch() {
 function disableCards() {
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
 }
 
 function unflipCards() {
+    lockBoard = true;
     setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
+
+        resetBoard();
     }, 1000);
 }
+
+function resetBoard() {
+    hasFlippedCard = false;
+    lockBoard = false;
+    firstCard = null;
+    secondCard = null;
+}
+//shuffle cards
+
+
+(function shuffle() {
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 18);
+        card.style.order = randomPos;
+    });
+})();
 
 cards.forEach(card => card.addEventListener('click', flipCard));
